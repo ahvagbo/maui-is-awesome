b5aab233a59c2d844c280cf65a7071a0fa8cd6c4
Iurie Jurja <iuriejurja@hotmail.com>
2023-05-25 23:17:31 +0300

    Initial commit

---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Data;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear()
+        {
+            ResultLabel.Text = string.Empty;
+        }
+
+        private void Calculate()
+        {
+            string expression = ResultLabel.Text.Replace("%", "/100")
+                                                .Replace('\u00d7', '*')
+                                                .Replace('\u00f7', '/');
+            DataTable dt = new DataTable();
+            decimal expr = Convert.ToDecimal(dt.Compute(expression, ""));
+            ExpressionLabel.Text = ResultLabel.Text;
+            if ((expr % 1) == 0) ResultLabel.Text = expr.ToString("G29").Replace(',', '.');
+            else ResultLabel.Text = expr.ToString().Replace(',', '.');
+        }
+
+        private void Backspace()
+        {
+            if (ResultLabel.Text == string.Empty
+             || ResultLabel.Text == "0")
+                ResultLabel.Text = "0";
+            else
+            {
+                ResultLabel.Text = ResultLabel.Text.Substring(0, ResultLabel.Text.Length - 1);
+                if (ResultLabel.Text.Length == 0)
+                    ResultLabel.Text = "0";
+            }
+        }
+
+        private void CalculatorBtn_OnPressed(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (ResultLabel.Text == "0") Clear();
+
+            if (btn.Text == "=") Calculate();
+            else if (btn.Text == "\u232b") Backspace();
+            else if (btn.Text == "()")
+            {
+                if (ResultLabel.Text == string.Empty)
+                    ResultLabel.Text = "(";
+
+                else
+                {
+                    if (char.IsDigit(ResultLabel.Text.Last())
+                     || ResultLabel.Text.Last() == ')') ResultLabel.Text += ")";
+                    else ResultLabel.Text += "(";
+                }
+            }
+            else if (btn.Text == "C")
+            {
+                ExpressionLabel.Text = string.Empty;
+                ResultLabel.Text = "0";
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(ExpressionLabel.Text)) ExpressionLabel.Text = string.Empty;
+                ResultLabel.Text += btn.Text;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# MAUI Is Awesome
+
+This repo contains some sample programs written in C# using the .NET MAUI platform.
+
+- [x] Calculator
+- [ ] Image Gallery
+- [ ] Browser
+- [ ] Car Shop
+- [ ] Memory Game
+
+This is the plan for now. More samples will be proposed and added in the future.
+
+## Can i contribute?
+Yes. All you need is just .NET 8 (preview), Visual Studio 2022 Preview with MAUI workload installed and Git. You know the drill.
+
+## Licensing
+MAUI Is Awesome is licensed under the terms and conditions of the MIT License.
