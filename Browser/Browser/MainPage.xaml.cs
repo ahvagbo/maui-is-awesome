1fc30f887930f00ea91c11d2d29ecb484204ece8
Iurie Jurja <iuriejurja@hotmail.com>
2023-05-28 00:34:31 +0300

    Modify MainPage.xaml.cs
    Added event handlers for buttons and the WebView (after navigation).

--- Browser/Browser/MainPage.xaml.cs
+++ Browser/Browser/MainPage.xaml.cs
@@ -5,6 +5,47 @@
         public MainPage()
         {
             InitializeComponent();
+
+            if (!BrowserWebView.CanGoBack)
+                BackBtn.IsEnabled = false;
+            if (!BrowserWebView.CanGoForward)
+                ForwardBtn.IsEnabled = false;
+        }
+
+        private void BrowserWebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            if (BrowserWebView.CanGoBack)
+                BackBtn.IsEnabled = true;
+            else
+                BackBtn.IsEnabled = false;
+
+            if (BrowserWebView.CanGoForward)
+                ForwardBtn.IsEnabled = true;
+            else
+                ForwardBtn.IsEnabled = false;
+        }
+
+        private void BackBtn_Pressed(object sender, EventArgs e)
+        {
+            BrowserWebView.GoBack();
+        }
+
+        private void ForwardBtn_Pressed(object sender, EventArgs e)
+        {
+            BrowserWebView.GoForward();
+        }
+
+        private void RefreshBtn_Pressed(object sender, EventArgs e)
+        {
+            BrowserWebView.Reload();
+        }
+
+        private void GoBtn_Pressed(object sender, EventArgs e)
+        {
+            BrowserWebView.Source = new UrlWebViewSource()
+            {
+                Url = UrlEntry.Text
+            };
         }
     }
 }
